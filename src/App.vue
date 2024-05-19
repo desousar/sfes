33e5f7ed746e66adeb17ae138f72824a46dce786
Benoit De Sousa <benoitdesousa@gmail.com>
2024-05-19 14:46:50 +0200

    App.vue script setup

--- src/App.vue
+++ src/App.vue
@@ -5,25 +5,23 @@ https://medium.com/fbbd/intro-to-writing-undo-redo-systems-in-javascript-af17148
 
 <template>
   <div id="app">
-    <!--<img alt="Vue logo" src="./assets/logo.png"> => explain how to import a picture-->
-    <!-- v-bind:abc <=> :abc AND v-on:abc <=> @abc -->
-    <MenuBar :currentLanguage="currentLanguage" />
+    <MenuBar :currentLanguage="state.currentLanguage" />
     <ToolBar
-      :currentLanguage="currentLanguage"
-      :selectedTool="tool"
+      :currentLanguage="state.currentLanguage"
+      :selectedTool="state.tool"
       @tool-state-changed="onToolStateChanged"
       @set-circuit="setCircuit"
-      :undoRedoData="undoRedoData"
+      :undoRedoData="state.undoRedoData"
       @set-position="setPosition"
     />
     <BigBody
-      :currentLanguage="currentLanguage"
-      :withPredefinedValue="withPredefinedValue"
-      :selectedTool="tool"
+      :currentLanguage="state.currentLanguage"
+      :withPredefinedValue="state.withPredefinedValue"
+      :selectedTool="state.tool"
       @tool-state-changed="onToolStateChanged"
-      :theCircuit="theCircuit"
+      :theCircuit="state.theCircuit"
       @set-circuit="setCircuit"
-      :undoRedoData="undoRedoData"
+      :undoRedoData="state.undoRedoData"
       @set-position="setPosition"
       @shift-history="shiftHistory"
       @slice-history="sliceHistory"
@@ -33,221 +31,202 @@ https://medium.com/fbbd/intro-to-writing-undo-redo-systems-in-javascript-af17148
     <!-- Pop Up Windows -->
 
     <popupAboutUs
-      v-show="isPopupAboutUsVisible"
+      v-show="state.isPopupAboutUsVisible"
       @close="openClosePopupAboutUs"
-      :currentLanguage="currentLanguage"
+      :currentLanguage="state.currentLanguage"
     />
 
     <popupSettings
-      v-show="isPopupSettingsVisible"
+      v-show="state.isPopupSettingsVisible"
       @close="openClosePopupSettings"
-      :currentLanguage="currentLanguage"
-      :locales="locales"
-      :withPredefinedValue="withPredefinedValue"
+      :currentLanguage="state.currentLanguage"
+      :locales="state.locales"
+      :withPredefinedValue="state.withPredefinedValue"
     />
 
     <popupComp
-      v-show="isPopupCompVisible"
+      v-show="state.isPopupCompVisible"
       @close="openClosePopupComp"
-      :theCompoToPass="theCompoToPass"
-      :arrayComponents="theCircuit.components"
-      :currentLanguage="currentLanguage"
-      :isPopupCompVisible="isPopupCompVisible"
+      :theCompoToPass="state.theCompoToPass"
+      :arrayComponents="state.theCircuit.components"
+      :currentLanguage="state.currentLanguage"
+      :isPopupCompVisible="state.isPopupCompVisible"
     />
 
     <popupResult
-      v-show="isPopupResultVisible"
+      v-show="state.isPopupResultVisible"
       @close="openClosePopupResult"
-      :arrayComponents="theCircuit.components"
-      :currentLanguage="currentLanguage"
-      :isPopupResultVisible="isPopupResultVisible"
+      :arrayComponents="state.theCircuit.components"
+      :currentLanguage="state.currentLanguage"
+      :isPopupResultVisible="state.isPopupResultVisible"
     />
 
     <popupEquivalentSrc
-      v-show="isPopupEquivalentSrcVisible"
+      v-show="state.isPopupEquivalentSrcVisible"
       @close="openClosePopupEquivalentSrc"
-      :circuitcomplet="theCircuit"
-      :currentLanguage="currentLanguage"
+      :circuitcomplet="state.theCircuit"
+      :currentLanguage="state.currentLanguage"
     />
 
     <popupHelp
-      v-show="isPopupHelpVisible"
+      v-show="state.isPopupHelpVisible"
       @close="openClosePopupHelp"
-      :currentLanguage="currentLanguage"
+      :currentLanguage="state.currentLanguage"
     />
   </div>
 </template>
 
-<script>
-import MenuBar from './components/MenuBar.vue';
-import ToolBar from './components/ToolBar.vue';
-import BigBody from './components/BigBody.vue';
+<script setup>
+import { reactive, onMounted } from 'vue';
 
-import popupAboutUs from './components/popupAboutUs.vue';
-import popupSettings from './components/popupSettings.vue';
-import popupComp from './components/popupComp.vue';
-import popupResult from './components/popupResult.vue';
-import popupEquivalentSrc from './components/popupEquivalentSrc.vue';
-import popupHelp from './components/popupHelp.vue';
+import MenuBar from '@/components/MenuBar.vue';
+import ToolBar from '@/components/ToolBar.vue';
+import BigBody from '@/components/BigBody.vue';
 
-import toolStates from './states.js';
-import EventBus from './components/jsFolder/event-bus';
+import popupAboutUs from '@/components/popupAboutUs.vue';
+import popupSettings from '@/components/popupSettings.vue';
+import popupComp from '@/components/popupComp.vue';
+import popupResult from '@/components/popupResult.vue';
+import popupEquivalentSrc from '@/components/popupEquivalentSrc.vue';
+import popupHelp from '@/components/popupHelp.vue';
+
+import toolStates from '@/states.js';
+import EventBus from '@/components/jsFolder/event-bus';
 
 import Circuit from '@/components/CircuitClass/Circuit.js';
 
 import { resetNbFromComp } from '@/components/CircuitClass/handleComponent/CreateComponent.js';
 
-export default {
-  name: 'App',
-  components: {
-    MenuBar,
-    ToolBar,
-    BigBody,
+onMounted(() => {
+  EventBus.on('PUSchangeLanguage', (newL) => {
+    updateLanguage(newL);
+  });
+  EventBus.on('MBaboutUs', () => {
+    openClosePopupAboutUs();
+  });
+  EventBus.on('MBsettings', () => {
+    openClosePopupSettings();
+  });
+  EventBus.on('MBequivalentSource', () => {
+    openClosePopupEquivalentSrc();
+  });
+  EventBus.on('MBhelp', () => {
+    openClosePopupHelp();
+  });
+  EventBus.on('PUSpredVal', (predValue) => {
+    state.withPredefinedValue = predValue;
+  });
+  EventBus.on('BBcomp', (compToPass) => {
+    console.log('modif', compToPass.name);
+    state.theCompoToPass = compToPass;
+    openClosePopupComp();
+  });
+  EventBus.on('BBresult', () => {
+    openClosePopupResult();
+  });
+  EventBus.on('BBSave', () => {
+    save();
+  });
+});
 
-    popupAboutUs,
-    popupSettings,
-    popupComp,
-    popupResult,
-    popupEquivalentSrc,
-    popupHelp
+const state = reactive({
+  currentLanguage: 'en', //default language
+  locales: [
+    { id: 'en', name: 'English' },
+    { id: 'de', name: 'Deutsch' }
+  ],
+  tool: toolStates.STATE_IDLE, //default first state
+  theCircuit: new Circuit([], []), //init a circuit
+  undoRedoData: {
+    // state history [older,...,younger]
+    history: [new Circuit([], [])], //1st state in history written manual
+    position: 0,
+    historyMaxLength: 100
   },
-  emits: ['tool-state-changed'],
-  mounted: function () {
-    EventBus.on('PUSchangeLanguage', (newL) => {
-      this.updateLanguage(newL);
-    });
-    EventBus.on('MBaboutUs', () => {
-      this.openClosePopupAboutUs();
-    });
-    EventBus.on('MBsettings', () => {
-      this.openClosePopupSettings();
-    });
-    EventBus.on('MBequivalentSource', () => {
-      this.openClosePopupEquivalentSrc();
-    });
-    EventBus.on('MBhelp', () => {
-      this.openClosePopupHelp();
-    });
-    EventBus.on('PUSpredVal', (predValue) => {
-      this.withPredefinedValue = predValue;
-    });
-    EventBus.on('BBcomp', (compToPass) => {
-      console.log('modif', compToPass.name);
-      this.theCompoToPass = compToPass;
-      this.openClosePopupComp();
-    });
-    EventBus.on('BBresult', () => {
-      this.openClosePopupResult();
-    });
-    EventBus.on('BBSave', () => {
-      this.save();
-    });
-  },
-  data() {
-    return {
-      currentLanguage: 'en', //default language
-      locales: [
-        { id: 'en', name: 'English' },
-        { id: 'de', name: 'Deutsch' }
-      ],
-      tool: toolStates.STATE_IDLE, //default first state
-      theCircuit: new Circuit([], []), //init a circuit
-      undoRedoData: {
-        // state history [older,...,younger]
-        history: [new Circuit([], [])], //1st state in history written manual
-        position: 0,
-        historyMaxLength: 100
-      },
 
-      isPopupAboutUsVisible: false,
-      isPopupSettingsVisible: false,
-      isPopupCompVisible: false,
-      isPopupResultVisible: false,
-      isPopupEquivalentSrcVisible: false,
-      isPopupHelpVisible: false,
+  isPopupAboutUsVisible: false,
+  isPopupSettingsVisible: false,
+  isPopupCompVisible: false,
+  isPopupResultVisible: false,
+  isPopupEquivalentSrcVisible: false,
+  isPopupHelpVisible: false,
 
-      withPredefinedValue: true,
-      theCompoToPass: null
-    };
-  },
-  methods: {
-    updateLanguage: function (updatedLanguage) {
-      this.currentLanguage = updatedLanguage;
-    },
+  withPredefinedValue: true,
+  theCompoToPass: null
+});
 
-    onToolStateChanged(nextState) {
-      this.tool = nextState;
-    },
-    setPosition(val) {
-      this.undoRedoData.position += val;
-    },
-    setCircuit(newCir) {
-      if (newCir) {
-        this.theCircuit = newCir;
-      } else {
-        this.theCircuit = new Circuit([], []);
-        //** Restart all nb drop on 0
-        resetNbFromComp();
-      }
-    },
-    /**
-     * #region Undo Redo function
-     */
-    save() {
-      const deepCopy = this.theCircuit.project();
-      this.setValue(deepCopy);
-    },
-    setValue(value) {
-      if (this.undoRedoData.position < this.undoRedoData.history.length - 1) {
-        this.sliceHistory(this.undoRedoData.position + 1);
-      }
-      if (
-        this.undoRedoData.position ===
-        this.undoRedoData.historyMaxLength - 1
-      ) {
-        this.shiftHistory();
-        this.setPosition(-1);
-      }
-      this.pushHistory(value);
-      this.setPosition(1);
-    },
-    shiftHistory() {
-      this.undoRedoData.history.shift();
-    },
-    sliceHistory(pos) {
-      this.undoRedoData.history = this.undoRedoData.history.slice(0, pos);
-    },
-    pushHistory(val) {
-      this.undoRedoData.history.push(val);
-    },
-    /**
-     * #endregion
-     */
+function updateLanguage(updatedLanguage) {
+  state.currentLanguage = updatedLanguage;
+}
 
-    openClosePopupAboutUs: function () {
-      this.isPopupAboutUsVisible = !this.isPopupAboutUsVisible;
-    },
-    openClosePopupSettings() {
-      this.isPopupSettingsVisible = !this.isPopupSettingsVisible;
-    },
-    openClosePopupComp: function () {
-      // situation: click on close button from pop up windows
-      if (this.isPopupCompVisible) {
-        this.save();
-      }
-      this.isPopupCompVisible = !this.isPopupCompVisible;
-    },
-    openClosePopupResult() {
-      this.isPopupResultVisible = !this.isPopupResultVisible;
-    },
-    openClosePopupEquivalentSrc: function () {
-      this.isPopupEquivalentSrcVisible = !this.isPopupEquivalentSrcVisible;
-    },
-    openClosePopupHelp: function () {
-      this.isPopupHelpVisible = !this.isPopupHelpVisible;
-    }
+function onToolStateChanged(nextState) {
+  state.tool = nextState;
+}
+function setPosition(val) {
+  state.undoRedoData.position += val;
+}
+function setCircuit(newCir) {
+  if (newCir) {
+    state.theCircuit = newCir;
+  } else {
+    state.theCircuit = new Circuit([], []);
+    //** Restart all nb drop on 0
+    resetNbFromComp();
   }
-};
+}
+/**
+ * #region Undo Redo function
+ */
+function save() {
+  const deepCopy = state.theCircuit.project();
+  setValue(deepCopy);
+}
+function setValue(value) {
+  if (state.undoRedoData.position < state.undoRedoData.history.length - 1) {
+    sliceHistory(state.undoRedoData.position + 1);
+  }
+  if (state.undoRedoData.position === state.undoRedoData.historyMaxLength - 1) {
+    shiftHistory();
+    setPosition(-1);
+  }
+  pushHistory(value);
+  setPosition(1);
+}
+function shiftHistory() {
+  state.undoRedoData.history.shift();
+}
+function sliceHistory(pos) {
+  state.undoRedoData.history = state.undoRedoData.history.slice(0, pos);
+}
+function pushHistory(val) {
+  state.undoRedoData.history.push(val);
+}
+/**
+ * #endregion
+ */
+
+function openClosePopupAboutUs() {
+  state.isPopupAboutUsVisible = !state.isPopupAboutUsVisible;
+}
+function openClosePopupSettings() {
+  state.isPopupSettingsVisible = !state.isPopupSettingsVisible;
+}
+function openClosePopupComp() {
+  // situation: click on close button from pop up windows
+  if (state.isPopupCompVisible) {
+    save();
+  }
+  state.isPopupCompVisible = !state.isPopupCompVisible;
+}
+function openClosePopupResult() {
+  state.isPopupResultVisible = !state.isPopupResultVisible;
+}
+function openClosePopupEquivalentSrc() {
+  state.isPopupEquivalentSrcVisible = !state.isPopupEquivalentSrcVisible;
+}
+function openClosePopupHelp() {
+  state.isPopupHelpVisible = !state.isPopupHelpVisible;
+}
 </script>
 
 <style>

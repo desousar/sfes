9285f34297513eac6bc97b22ea4d84cc138e81b4
Benoit De Sousa <benoitdesousa@gmail.com>
2024-05-19 15:29:53 +0200

    ToolBar.vue script setup

--- src/components/ToolBar.vue
+++ src/components/ToolBar.vue
@@ -4,164 +4,158 @@
       <div
         class="content_img"
         v-bind:class="{
-          greenClass: selectedTool === toolState.TOOL_DELETE
+          greenClass: props.selectedTool === state.toolState.TOOL_DELETE
         }"
       >
         <button @click="deleteItem">
           <span class="mdi mdi-delete mdi-48px"></span>
         </button>
-        <div>{{ deleteIcon[getCurrentLanguage] }}</div>
+        <div>{{ language.deleteIcon[getCurrentLanguage] }}</div>
       </div>
       <div
         class="content_img"
         v-bind:class="{
-          greenClass: selectedTool === toolState.TOOL_ROTATE
+          greenClass: props.selectedTool === state.toolState.TOOL_ROTATE
         }"
       >
         <button @click="rotateItem">
           <span class="mdi mdi-rotate-right mdi-48px"></span>
         </button>
-        <div>{{ rotate[getCurrentLanguage] }}</div>
+        <div>{{ language.rotate[getCurrentLanguage] }}</div>
       </div>
       <div
         class="content_img"
         v-bind:class="{
-          greenClass: selectedTool === toolState.TOOL_MOVE
+          greenClass: props.selectedTool === state.toolState.TOOL_MOVE
         }"
       >
         <button @click="movementItem">
           <span class="mdi mdi-cursor-move mdi-48px"></span>
         </button>
-        <div>{{ movement[getCurrentLanguage] }}</div>
+        <div>{{ language.movement[getCurrentLanguage] }}</div>
       </div>
       <div
         class="content_img"
         v-bind:class="{
-          greenClass: selectedTool === toolState.TOOL_SELECT
+          greenClass: props.selectedTool === state.toolState.TOOL_SELECT
         }"
       >
         <button @click="selectItem">
           <span class="mdi mdi-select mdi-48px"></span>
         </button>
-        <div>{{ select[getCurrentLanguage] }}</div>
+        <div>{{ language.select[getCurrentLanguage] }}</div>
       </div>
       <div
         class="content_img"
         v-bind:class="{
-          greenClass: selectedTool === toolState.STATE_IDLE
+          greenClass: props.selectedTool === state.toolState.STATE_IDLE
         }"
       >
         <button @click="mouseItem">
           <span class="mdi mdi-cursor-default-outline mdi-48px"></span>
         </button>
-        <div>{{ mouse[getCurrentLanguage] }}</div>
+        <div>{{ language.mouse[getCurrentLanguage] }}</div>
       </div>
     </div>
     <div class="third">
       <div class="content_img">
-        <button @click="undo" :disabled="!canUndo">
+        <button
+          @click="undo"
+          :disabled="!canUndo"
+        >
           <span class="mdi mdi-undo mdi-24px"></span>
         </button>
-        <div>{{ undo_data[getCurrentLanguage] }}</div>
+        <div>{{ language.undo_data[getCurrentLanguage] }}</div>
       </div>
       <div class="content_img">
-        <button @click="redo" :disabled="!canRedo">
+        <button
+          @click="redo"
+          :disabled="!canRedo"
+        >
           <span class="mdi mdi-redo mdi-24px"></span>
         </button>
-        <div>{{ redo_data[getCurrentLanguage] }}</div>
+        <div>{{ language.redo_data[getCurrentLanguage] }}</div>
       </div>
     </div>
   </div>
 </template>
 
-<script>
-import toolStates from '../states.js';
+<script setup>
+import { reactive, computed, defineProps, defineEmits } from 'vue';
 
-export default {
-  //name: 'TitleBanner',
-  props: {
-    //variable, who I obtain from App.vue (one stage up of this stage)
-    //I need to give the type, after that I can us it into html part like {{...}}
-    currentLanguage: String,
-    selectedTool: Number,
-    undoRedoData: Object
-  },
-  emits: ['click', 'tool-state-changed', 'set-position', 'set-circuit'],
-  data() {
-    return {
-      neu: { en: 'New', de: 'Neu' },
-      open: { en: 'Open', de: 'Öffnen' },
-      saveAs: { en: 'Save As', de: 'Speichern als' },
-      copy: { en: 'Copy', de: 'Kopieren' },
-      cut: { en: 'Cut', de: 'Schneiden' },
-      paste: { en: 'Paste', de: 'Einfügen' },
-      deleteIcon: { en: 'Delete', de: 'Löschen' },
-      rotate: { en: 'Rotate', de: 'Drehen' },
-      movement: { en: 'Move', de: 'Bewegen' },
-      select: { en: 'Select', de: 'Wählen' },
-      mouse: { en: 'Edit', de: 'Bearbeiten' },
-      undo_data: { en: 'Undo', de: 'Undo' },
-      redo_data: { en: 'Redo', de: 'Redo' },
+import toolStates from '@/states.js';
 
-      toolState: toolStates
-    };
-  },
-  computed: {
-    getCurrentLanguage: function() {
-      return this.currentLanguage;
-    },
-    canUndo: function() {
-      return this.undoRedoData.position > 0;
-    },
-    canRedo: function() {
-      return (
-        this.undoRedoData.history.length - 1 - this.undoRedoData.position > 0
-      );
-    }
-  },
-  methods: {
-    deleteItem: function() {
-      this.$emit('tool-state-changed', this.toolState.TOOL_DELETE);
-    },
-    rotateItem: function() {
-      this.$emit('tool-state-changed', this.toolState.TOOL_ROTATE);
-    },
-    movementItem: function() {
-      this.$emit('tool-state-changed', this.toolState.TOOL_MOVE);
-    },
-    selectItem: function() {
-      this.$emit('tool-state-changed', this.toolState.TOOL_SELECT);
-    },
-    mouseItem: function() {
-      this.$emit('tool-state-changed', this.toolState.STATE_IDLE);
-    },
-    // Undo and Redo
-    undo() {
-      if (this.undoRedoData.position > 0) {
-        this.$emit('set-position', -1);
-      }
-      let deepcopy = this.undoRedoData.history[
-        this.undoRedoData.position
-      ].project();
-      deepcopy.components.forEach(element => {
-        element.recalculatePins();
-      });
-      this.$emit('set-circuit', deepcopy);
-    },
-    redo() {
-      if (this.undoRedoData.position < this.undoRedoData.history.length - 1) {
-        this.$emit('set-position', 1);
-      }
-      let deepcopy = this.undoRedoData.history[
-        this.undoRedoData.position
-      ].project();
-      deepcopy.components.forEach(element => {
-        element.recalculatePins();
-      });
-      this.$emit('set-circuit', deepcopy);
-    }
+const props = defineProps({
+  currentLanguage: String,
+  selectedTool: Number,
+  undoRedoData: Object
+});
+
+const emit = defineEmits(['tool-state-changed', 'set-position', 'set-circuit']);
+
+const state = reactive({
+  toolState: toolStates
+});
+const language = reactive({
+  deleteIcon: { en: 'Delete', de: 'Löschen' },
+  rotate: { en: 'Rotate', de: 'Drehen' },
+  movement: { en: 'Move', de: 'Bewegen' },
+  select: { en: 'Select', de: 'Wählen' },
+  mouse: { en: 'Edit', de: 'Bearbeiten' },
+  undo_data: { en: 'Undo', de: 'Undo' },
+  redo_data: { en: 'Redo', de: 'Redo' }
+});
+
+const getCurrentLanguage = computed(() => {
+  return props.currentLanguage;
+});
+const canUndo = computed(() => {
+  return props.undoRedoData.position > 0;
+});
+const canRedo = computed(() => {
+  return (
+    props.undoRedoData.history.length - 1 - props.undoRedoData.position > 0
+  );
+});
+
+function deleteItem() {
+  emit('tool-state-changed', state.toolState.TOOL_DELETE);
+}
+function rotateItem() {
+  emit('tool-state-changed', state.toolState.TOOL_ROTATE);
+}
+function movementItem() {
+  emit('tool-state-changed', state.toolState.TOOL_MOVE);
+}
+function selectItem() {
+  emit('tool-state-changed', state.toolState.TOOL_SELECT);
+}
+function mouseItem() {
+  emit('tool-state-changed', state.toolState.STATE_IDLE);
+}
+// Undo and Redo
+function undo() {
+  if (props.undoRedoData.position > 0) {
+    emit('set-position', -1);
   }
-};
+  let deepcopy =
+    props.undoRedoData.history[props.undoRedoData.position].project();
+  deepcopy.components.forEach((element) => {
+    element.recalculatePins();
+  });
+  emit('set-circuit', deepcopy);
+}
+function redo() {
+  if (props.undoRedoData.position < props.undoRedoData.history.length - 1) {
+    emit('set-position', 1);
+  }
+  let deepcopy =
+    props.undoRedoData.history[props.undoRedoData.position].project();
+  deepcopy.components.forEach((element) => {
+    element.recalculatePins();
+  });
+  emit('set-circuit', deepcopy);
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -171,13 +165,6 @@ export default {
   background-color: #f2f2f2;
   height: 50px;
   margin-bottom: 20px;
-}
-.first {
-  display: flex;
-}
-.second {
-  margin-left: 40px;
-  display: flex;
 }
 .third {
   margin-left: 40px;
